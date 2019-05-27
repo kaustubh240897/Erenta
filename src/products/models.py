2f56f04572fa1c3564bf98c5f42412d326a71576
unknown <kaustubh.dwi@gmail.com>
2019-05-27 13:12:51 +0530

    added productmanager and active component

--- src/products/models.py
+++ src/products/models.py
@@ -19,13 +19,34 @@ def upload_image_path(instance, filename):
         final_filename=final_filename
         )
 
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+       return self.filter(active=True)
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().active()
+
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id) # products_description.objects
+        if qs.count()==1:
+            return qs.first
+        return None
 
 class Product_description(models.Model):
     product_name = models.CharField(max_length=100)
     description = models.TextField()
     quantity = models.CharField(max_length=10, default=1) 
-    cost = models.DecimalField(max_digits=15, decimal_places=2 , null=True)
+    cost_per_day = models.DecimalField(max_digits=15, decimal_places=2 , null=True)
+    days = models.IntegerField( null=True)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    active = models.BooleanField(default=True)
+
+    objects = ProductManager()
 
     def __str__(self):
         return self.product_name
--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView,DetailView
 from django.shortcuts import render, get_object_or_404
 from .models import Product_description
+from django.http import Http404
 
 # Create your views here.
 def product_list_view(request):
@@ -11,8 +12,11 @@ def product_list_view(request):
     return render(request,"products/product_list.html", context)
 
 def product_detail_view(request, pk=None, *args, **kwargs):
-    #instance = Product_description.objects.get(pk=pk)
-    instance = get_object_or_404(Product_description, pk=pk)
+    
+    instance = Product_description.objects.get_by_id(pk)
+    if instance is None:
+        raise Http404("product doesnot exist")
+
     context = {
         'object': instance
     }
