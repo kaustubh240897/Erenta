ca3f09547180ecb7d463ba825a9b63116d4e2974
unknown <kaustubh.dwi@gmail.com>
2019-05-31 13:44:25 +0530

    Making the search component

--- src/search/views.py
+++ src/search/views.py
@@ -15,4 +15,10 @@ def search_product_view(request):
           'qs': queryset ,
          "title":"Products",
     }
-    return render(request,"products/product_list.html", context)
+    def get_context_data(self,*args, **kwargs):
+        context=super(search_product_view ,self).get_context_data(*args, **kwargs)
+        query=self.request.GET.get('q')
+        context['query']=query
+        #SearchQuery.objects.create(query=query)
+
+    return render(request,"search/view.html", context)
