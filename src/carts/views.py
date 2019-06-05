9013987304df19c670611165ebd2e5a4e5225448
unknown <kaustubh.dwi@gmail.com>
2019-06-05 10:20:48 +0530

    Cart Manager

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render
 from .models import Cart
+
 # Create your views here.
 
 
 
 
 def cart_home(request):
-    cart_id = request.session.get("cart_id", None)
-    qs=Cart.objects.filter(id=cart_id)
-    if qs.count() ==1:
-        cart_obj=qs.first()
-        if request.user.is_authenticated and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj= Cart.objects.new(user=request.user)
-        request.session['cart_id'] = cart_obj.id
-        
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = cart_obj.products.all()
+    total=0
+    for x in products:
+        total += x.cost_per_day
+    print(total)
+    cart_obj.total=total
+    cart_obj.save()
     return render(request,"carts/home.html", {})
 
