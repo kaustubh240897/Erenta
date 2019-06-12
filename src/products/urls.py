dbe43ee23f3de71726bdafd8cfbff79e2a380bc7
unknown <kaustubh.dwi@gmail.com>
2019-06-12 22:13:11 +0530

    added category in buttons

--- src/products/urls.py
+++ src/products/urls.py
@@ -4,11 +4,17 @@
 from django.contrib import admin
 from django.urls import path
 from .views import product_list_view,ProductDetailSlugView
+from .views import catogary_product_view_1
+from .views import catogary_product_view_2
+from .views import catogary_product_view_3
 
 
 urlpatterns = [
    
     path('',product_list_view, name='list'),
-    path('<slug:slug>/',ProductDetailSlugView.as_view(), name='detail')
+    path('<slug:slug>/',ProductDetailSlugView.as_view(), name='detail'),
+    path('clothing',catogary_product_view_1 , name='query'),
+    path('Novels',catogary_product_view_2 , name='query_1'),
+    path('instruments',catogary_product_view_3 , name='query_3'),
 ]
 
