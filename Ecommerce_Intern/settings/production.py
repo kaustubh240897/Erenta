95c2e48148b9ebcbb86f1d155fd38eee8191d329
unknown <kaustubh.dwi@gmail.com>
2019-07-05 15:33:49 +0530

    .

--- Ecommerce_Intern/settings/production.py
+++ Ecommerce_Intern/settings/production.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-import django_heroku
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -131,7 +131,7 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['CONN_MAX_AGE'] = 500
-DATABASES['default']['REQUIRE_SSL']= True
+
 
 
 # Password validation
@@ -171,8 +171,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#Activate django-heroku
-django_heroku.settings(locals())
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_project"),
