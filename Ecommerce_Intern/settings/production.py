10092f848684e1d5e1b5dac3071e67e6ee7ec012
unknown <kaustubh.dwi@gmail.com>
2019-07-05 14:58:38 +0530

    updated settings.py

--- Ecommerce_Intern/settings/production.py
+++ Ecommerce_Intern/settings/production.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -166,6 +166,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+#Activate django-heroku
+django_heroku.settings(locals())
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_project"),
 ]
