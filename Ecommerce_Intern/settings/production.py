126fc0dba8b56ce05136bb1b85eb388c902e438d
ディヴィディカウスタブ <k.dwivedi@pro-s.co.jp>
2021-02-05 22:44:42 +0530

    r

--- Ecommerce_Intern/settings/production.py
+++ Ecommerce_Intern/settings/production.py
@@ -209,3 +209,12 @@ MEDIA_ROOT=os.path.join(os.path.dirname(BASE_DIR), "static_files", "media_root")
 
 from Ecommerce_Intern.aws.conf import *
 
+CORS_REPLACE_HTTPS_REFERER      = True
+HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
